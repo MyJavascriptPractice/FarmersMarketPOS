c8ee92c69e7f6a27a7946706bfec46bf430c3a2a
will <willsmith12289@gmail.com>
2016-02-02 11:44:02 -0500

    first

---
+++ js/index.js
@@ -0,0 +1,34 @@
+var weight=3;
+var num = 1;
+
+function veg(num, price, weight, id) {
+	this.num = num;
+	this.price = price;
+	this.weight = weight;
+	this.id = id;
+}
+
+var produce = [];
+
+var tomato = new veg(null, 3.50, weight, "tomato");
+var bunch = new veg (num, 3, null, bunch);
+var potato = new veg(null, 2, weight);
+var pepper = new veg(null, 2.50, weight);
+var onion = new veg(null, 2, weight);
+var lettuce = new veg(num, 3, null);
+var herb = new veg(num, 2, null);
+var car = new veg(num, 3, null);
+var bag = new veg(num, 3.50, null);
+
+produce.push("tomato", "bunch", "potato", "pepper", "onion", "lettuce", "herb", "car", "bag");
+
+function findId() {
+	$('img').click(function() {
+ 		($(this).attr("id")).valueof()});
+ 	};
+
+ console.log(findId)
+console.log(findId(this))
+function weightprice	
+	if (#id===produce[3]);
+		then console.log("true")
